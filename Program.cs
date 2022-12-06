81947383feec426b1791b5c0237b6f140963e478
Ysmael Braga <ysmael.braga@ibm.com>
2022-12-06 00:30:29 -0300

    lendo um arquivo em c#

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+
+namespace ReadFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Classe criada para passar o path do arquivo
+            FilesTarget filesTarget = new FilesTarget();
+
+            string sourcePath = filesTarget.UpdateSourcePath();
+            string targetPath = filesTarget.UpdateTargetPath();
+         
+            try
+            {
+              
+                using (StreamReader sr = File.OpenText(sourcePath))
+
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string? line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+
+            }
+            catch (IOException error)
+            {
+                Console.WriteLine("An error occureed");
+                Console.WriteLine(error.Message);
+            }
+        }
+    }
+}
